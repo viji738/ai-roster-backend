31eebb4f1f3e6ba22ebe7e7617aea6ac7c5bf3b2
viji738 <balabala400044@gmail.com>
2026-08-20 14:18:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -140,10 +140,8 @@ app.get("/", (req, res) => {
 // SERVER
 // ==========================================
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-    console.log(
-        "Server running on http://localhost:5000"
-    );
-
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
